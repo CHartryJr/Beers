1405529a7f127f3aa53b8b6ac13c03bce9634d60
Carl Hartry Jr <carlhartry+github@gmail.com>
2023-10-29 20:35:20 -0400

    test update

--- Test.sql
+++ Test.sql
@@ -45,30 +45,54 @@
 -- -- Write a query that returns the addresses of the bars a specific drinker such as “Dan” frequents.
 -- -- Find the Addresses of the Bars
 
--- SELECT D.FNAME,D.LNAME,VIS_B.BAR_NAME, VIS_B.ADDRESS  
+-- SELECT D.FNAME || ' '|| D.LNAME AS VISTOR ,VIS_B.BAR_NAME, VIS_B.ADDRESS  
 -- FROM DRINKER AS D JOIN (SELECT V.PERSON,B.NAME AS BAR_NAME,B.ADDRESS FROM VISITS as V join  BAR AS B  ON  V.BAR_VISITED = B.BAR_ID ) AS VIS_B
 -- ON D.DRINKER_ID = VIS_B.PERSON  GROUP BY  D.FNAME, D.LNAME, VIS_B.BAR_NAME, VIS_B.ADDRESS;  
 
 -- RESULT
--- FNAME|LNAME|BAR_NAME|ADDRESS
--- BEN|BENJERMAN|SATISFACTION|905 W.MAIN STREET
--- DAN|DANGER|SATISFACTION|905 W.MAIN STREET
--- DAN|DANGER|THE EDGE|108 MORRIS STREET
--- LARC|YRTRAH|CLUB BOBCAT|231 W Hancock STREET
--- LARC|YRTRAH|SATISFACTION|905 W.MAIN STREET
--- LARC|YRTRAH|THE EDGE|108 MORRIS STREET
+-- VISTOR|BAR_NAME|ADDRESS
+-- BEN BENJERMAN|SATISFACTION|905 W.MAIN STREET
+-- DAN DANGER|SATISFACTION|905 W.MAIN STREET
+-- DAN DANGER|THE EDGE|108 MORRIS STREET
+-- LARC YRTRAH|CLUB BOBCAT|231 W Hancock STREET
+-- LARC YRTRAH|SATISFACTION|905 W.MAIN STREET
+-- LARC YRTRAH|THE EDGE|108 MORRIS STREET
 
 
 -- Write a query that returns the names of all bars that serve a particular beer such as “Dixie”.
 -- Find the Names of Bars
 
+-- SELECT B.NAME AS Beer,B_SER.NAME AS 'Bar name' FROM BEER AS B JOIN (SELECT SERVES.*, BAR.Name FROM SERVES JOIN BAR ON BAR.BAR_ID  == SERVES.BAR_SELLING ) AS B_SER ON B.BEER_ID == B_SER.BEER_SOLD GROUP BY B.NAME,B_SER.NAME; 
+
+-- RESULT
+-- Beer|Bar name
+-- Budweiser|CLUB BOBCAT
+-- Budweiser|SATISFACTION
+-- Budweiser|THE EDGE
+-- Corona|CLUB BOBCAT
+-- Corona|THE EDGE
+-- Dixie|CLUB BOBCAT
+-- Miller Lite|CLUB BOBCAT
 
 -- Write a query that returns the drinkers who like one beer such as “Dixie” but not another beer such as “Corona”.
 -- Find the Drinkers
+ 
+-- SELECT B.NAME AS Beer, DRINKS.NAME AS 'Drinker that likes' FROM BEER AS B JOIN (SELECT LIKES.*, D.FNAME||' '||D.LNAME AS NAME FROM LIKES JOIN DRINKER AS D ON D.DRINKER_ID  ==  LIKES.PERSON) AS DRINKS ON B.BEER_ID == DRINKS.BEER_CHOICE GROUP BY B.NAME,DRINKS.NAME; 
 
+-- Results
+-- Beer|Drinker that likes
+-- Budweiser|BEN BENJERMAN
+-- Budweiser|DAN DANGER
+-- Budweiser|LARC YRTRAH
+-- Corona|AMY BRANCH
+-- Corona|DAN DANGER
+-- Corona|LARC YRTRAH
+-- Dixie|LARC YRTRAH
+-- Miller Lite|LARC YRTRAH
 
 -- Write a query that finds pairs of bars (bar1 and bar2 should be different) that have the same price on at least one beer. Rows returned should look like: [bar1, bar2, beer].
 -- Find Pairs of Bars
+
 
 
 -- Write a query that finds all drinkers who ONLY visit bars that serve a specific beer such as “Dixie”.
